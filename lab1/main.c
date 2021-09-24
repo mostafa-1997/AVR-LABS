a583d458b43447ff7318be776f49b1fcba0a2508
Mostafa <mostax266@gmail.com>
2021-09-24 22:58:05 +0200

    read from button

--- lab1/main.c
+++ lab1/main.c
@@ -15,34 +15,44 @@
 
 #define DDRB *((u8*)0x37)
 #define PORTB *((u8*)0x38)
+#define PINB *((u8*)0x39)
 
-int main(){
+#define DDRD	*((u8*)0x31)
+#define PORTD	*((u8*)0x32)
+#define PIND	*((u8*)0x30)
 
-	//PortC.7 DDR direction
-	DDRC |= (1<<7);
+u8 seg []= {	0x3f,0x06,0x5B,0x4F,0x66,
+				0x6D,0x7D,0x07,0x7F,0x6F,0x77,
+				0x7C,0x39,0x5E,0x79,0x71
+		   };
+int main()
+{
+	DDRC |=(1<<2);//led1
+	DDRC |=(1<<7);//led2
 
-	DDRA |= (0x0f<<4);//instead of making DDRA 4 times
-	//0b00001111<<4 = 0b11110000 | 0b00001010 =0b11111010
+	DDRD &=~(1<<6);//sw1
+	DDRD &=~(1<<0);//sw2
 
-	DDRB |= (1<<1);
+	//PORTA |=(1<<6);//pull up sw1
+	//PORTA |=(1<<7);//pull up sw2
 
-	//will print 5 in the hexadecoder the 4 selection pins
-	PORTA &=~(1<<7);
-	PORTA |= (1<<6);
-	PORTA &=~ (1<<5);
-	PORTA |= (1<<4);
-	//0b0101 = 5
+	while(1) //for(;;)
+	{
+		if ( ((PIND>>6)&1) == 1)
+		{
+			PORTC |=(1<<2); //led1 on
+			PORTC &=~(1<<7);//led2 off
+		}
+		else if ( ((PIND>>0)&1) == 1)
+		{
+			PORTC &=~(1<<2); //led1 off
+			PORTC &=~(1<<7);//led2 off
+		}
 
-	//7segment enable
-	PORTB |=(1<<2);
-
-
-	 while(1)
-	 {
-		 //PortC.7 output
-		 PORTC |= (1<<7);
-		 _delay_ms(1000);
-		 PORTC &= ~(1<<7);
-		 _delay_ms(1000);
-	 }
+		if ( ((PIND>>6)&1) == 0 && ((PIND>>0)&1) == 0)
+		{
+			PORTC |=(1<<7); //led2 on
+			PORTC &=~(1<<2);//led1 off
+		}
+	}
 }
