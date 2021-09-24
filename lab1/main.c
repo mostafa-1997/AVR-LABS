22feb708ac42182187571084b3c026de42aa0a0b
Mostafa <mostax266@gmail.com>
2021-09-24 19:59:36 +0200

    Blink LED

--- lab1/main.c
+++ lab1/main.c
@@ -5,12 +5,22 @@
  *      Author: sa7er
  */
 #include "stdTypes.h"
+#include <util/delay.h>
 
+#define DDRC *((u8*)0x34)
+#define PORTC *((u8*)0x35)
 int main(){
 
 	//PortC.7 DDR direction
-	*((u8*)0x34) |= (1<<7);
+	DDRC |= (1<<7);
 
-	//PortC.7 output
-	*((u8*)0x35) |= (1<<7);
+
+	 while(1)
+	 {
+		 //PortC.7 output
+		 PORTC |= (1<<7);
+		 _delay_ms(1000);
+		 PORTC &= ~(1<<7);
+		 _delay_ms(1000);
+	 }
 }
