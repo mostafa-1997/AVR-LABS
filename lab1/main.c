0ed7f59503b95dcf85559d8c2e71a42cb2bf6a1b
Mostafa <mostax266@gmail.com>
2021-09-24 21:22:37 +0200

    Interface 7 segment display using hexadecoder IC

--- lab1/main.c
+++ lab1/main.c
@@ -9,10 +9,32 @@
 
 #define DDRC *((u8*)0x34)
 #define PORTC *((u8*)0x35)
+
+#define DDRA *((u8*)0x3A)
+#define PORTA *((u8*)0x3B)
+
+#define DDRB *((u8*)0x37)
+#define PORTB *((u8*)0x38)
+
 int main(){
 
 	//PortC.7 DDR direction
 	DDRC |= (1<<7);
+
+	DDRA |= (0x0f<<4);//instead of making DDRA 4 times
+	//0b00001111<<4 = 0b11110000 | 0b00001010 =0b11111010
+
+	DDRB |= (1<<1);
+
+	//will print 5 in the hexadecoder the 4 selection pins
+	PORTA &=~(1<<7);
+	PORTA |= (1<<6);
+	PORTA &=~ (1<<5);
+	PORTA |= (1<<4);
+	//0b0101 = 5
+
+	//7segment enable
+	PORTB |=(1<<2);
 
 
 	 while(1)
